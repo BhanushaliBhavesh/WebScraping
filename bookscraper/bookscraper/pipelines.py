fc25f922bbee1448a9d98a826ebfe5b4e58aab3f
Peter <bhaveshbhanushali721@gmail.com>
2024-06-06 18:04:05 +0530

    Added functionality to save scraped data to database

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -64,4 +64,94 @@ class BookscraperPipeline:
         return item
     
 
+import mysql.connector
+import os
 
+class SaveToMySQLPipeline:
+
+    def __init__(self):
+        mysql_password = os.getenv('MYSQL_PASSWORD')
+        self.conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password=mysql_password,
+            database = 'books'
+        )
+
+        ## Create cursor, used to execute commands
+        self.cur = self.conn.cursor()
+
+        ## Create books table if none exists
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS books(
+            id int NOT NULL auto_increment, 
+            url VARCHAR(255),
+            title text,
+            upc VARCHAR(255),
+            product_type VARCHAR(255),
+            price_execluding_tax DECIMAL,
+            price_including_tax DECIMAL,
+            tax DECIMAL,
+            availablity INTEGER,
+            num_reviews INTEGER,
+            stars INTEGER,
+            category VARCHAR(255),
+            description text,
+            PRIMARY KEY (id)
+        )
+        """)
+
+    def process_item(self, item, spider):
+
+        ## Define insert statement
+        self.cur.execute(""" insert into books (
+            url, 
+            title, 
+            upc, 
+            product_type, 
+            price_execluding_tax,
+            price_including_tax,
+            tax,
+            availablity,
+            num_reviews,
+            stars,
+            category,
+            description
+            ) values (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+                )""", (
+            item["url"],
+            item["title"],
+            item["upc"],
+            item["product_type"],
+            item["price_execluding_tax"],
+            item["price_including_tax"],
+            item["tax"],
+            item["availablity"],
+            item["number_of_reviews"],
+            item["stars"],
+            item["category"],
+            str(item["product_description"])
+        ))
+
+        # ## Execute insert of data into database
+        self.conn.commit()
+        return item
+
+    
+    def close_spider(self, spider):
+
+        ## Close cursor & connection to database 
+        self.cur.close()
+        self.conn.close()
--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -71,6 +71,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "bookscraper.pipelines.BookscraperPipeline": 300,
+    "bookscraper.pipelines.SaveToMySQLPipeline": 400
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -98,3 +99,11 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+FEEDS = {
+
+    'test.csv':  {'format': 'csv'}
+
+
+}
